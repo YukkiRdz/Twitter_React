a423d56320a7609f7fc6498ec3459533b18bc7a2
YukkiRDZ <thiiagorodriguez.sk8@gmail.com>
2025-09-13 19:47:28 -0300

    Comienzo con useState

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+import img1 from './assets/Yuilustrarte.jpg'
+import img2 from './assets/Yukki.png'
+
+export function App () {
+    return (
+        <section className='App'>
+            <TwitterFollowCard avatar={img1} userName='yuilustrarte'>
+            Yuiri Miyagi
+            </TwitterFollowCard>
+            <TwitterFollowCard avatar={img2} userName='yukki'>
+            Thiago Nahuel Rodriguez
+            </TwitterFollowCard>
+        </section>
+    )
+}
+
---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react" //hook
+
+export function TwitterFollowCard ( { userName, children, avatar}) {
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing) //cambiar el estado, true a false o vicebersa
+        text = ''
+    }
+    return (
+                <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={avatar} alt={`Avatar de ${userName}`} />
+                <div className='tw-followCard-info'>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
+                </button>
+            </aside>
+        </article>
+    )
+}
